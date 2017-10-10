e7a7f0cf5a395619640bcf6129269778f4a95b50
Eirik Steinsland <Eirik@Eiriks-MacBook-Pro-2.local>
2017-10-10 13:29:09 +0200

    First commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+
+var greetingNumb = parseInt(Math.random()*3);
+
+if (greetingNumb == 1){
+    app.get('/greet/:id', function (req, res) {
+      res.send('Hello There '+ req.params.id + '!')
+    })
+}
+else if(greetingNumb == 2){
+    app.get('/greet/:id', function (req, res) {
+      res.send('Nice to see you '+ req.params.id + '!')
+    })
+}
+else{
+    app.get('/greet/:id', function (req, res) {
+      res.send('Greetings and salutations '+ req.params.id + '!')
+    })
+}
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 8080!')
+})
+
+//app.use(express.static('public'))
+
+
